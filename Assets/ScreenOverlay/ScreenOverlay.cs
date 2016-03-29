f8d925ddb167d0feb92f37173f50f2433da175f1
Keijiro Takahashi <keijiro@Ultra.jt.unity3d.com>
2016-03-29 15:55:57 +0900

    Add ScreenOverlay.

---
+++ Assets/ScreenOverlay/ScreenOverlay.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ScreenOverlay : MonoBehaviour
+{
+    [SerializeField]
+    Texture _mask;
+
+    [SerializeField]
+    Shader _shader;
+
+    Material _material;
+
+    void OnEnable()
+    {
+        _material = new Material(Shader.Find("Hidden/UnityLogo/ScreenOverlay"));
+        _material.hideFlags = HideFlags.DontSave;
+    }
+
+    void OnDisable()
+    {
+        DestroyImmediate(_material);
+        _material = null;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        _material.SetTexture("_MaskTex", _mask);
+        Graphics.Blit(source, destination, _material, 0);
+    }
+}
---
+++ Assets/ScreenOverlay/ScreenOverlay.shader
@@ -0,0 +1,36 @@
+﻿Shader "Hidden/UnityLogo/ScreenOverlay"
+{
+    Properties
+    {
+        _MainTex("", 2D) = "white"{}
+        _MaskTex("", 2D) = "white"{}
+    }
+
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+    sampler2D _MaskTex;
+
+    fixed4 frag (v2f_img i) : SV_Target
+    {
+        fixed4 c = tex2D(_MainTex, i.uv);
+        fixed m = tex2D(_MaskTex, i.uv).r;
+        return c * m;
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+            ENDCG
+        }
+    }
+}
